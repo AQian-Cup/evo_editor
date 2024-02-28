1cf149e817fd26c1dcd09a8fe4103972c9b7f6d3
AQian <970506352@qq.com>
2024-02-28 16:02:01 +0800

    feat: 实现代码高亮

--- src/components/tiptap.vue
+++ src/components/tiptap.vue
@@ -7,9 +7,22 @@ import { useEditor, EditorContent } from '@tiptap/vue-3';
 import StarterKit from '@tiptap/starter-kit';
 import CharacterCount from '@tiptap/extension-character-count';
 import Image from '@tiptap/extension-image';
+import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
+import { common, createLowlight } from 'lowlight';
+import 'highlight.js/styles/github-dark.min.css';
 
+const lowlight = createLowlight(common);
 const editor = useEditor({
-  extensions: [StarterKit, CharacterCount, Image],
+  extensions: [
+    StarterKit.configure({
+      codeBlock: false,
+    }),
+    CharacterCount,
+    Image,
+    CodeBlockLowlight.configure({
+      lowlight,
+    }),
+  ],
   content: '',
 });
 defineExpose({
